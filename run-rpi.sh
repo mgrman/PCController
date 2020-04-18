51b8cdcb41feaa22d65b94563b9a0a2f5495292f
martin_grman <martingrman@outlook.com>
2020-04-18 18:06:37 +0200

    config tests

--- run-rpi.sh
+++ run-rpi.sh
@@ -1,2 +1,3 @@
 export ASPNETCORE_ENVIRONMENT=rpi
-dotnet run --project "src/PCController.Local/PCController.Local.csproj"
+dotnet run --project "src/PCController.Local/PCController.Local.csproj" -- --urls "http://*:8080"
+read -p "Press enter to continue"
