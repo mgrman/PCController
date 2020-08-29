7ecdb94437dd151765cc44a7210df2ae7f467c2b
Martin Grman <martingrman@outlook.com>
2020-08-29 18:23:51 +0200

    server only http

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -29,18 +29,7 @@ namespace PCController.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
@@ -53,7 +42,6 @@ namespace PCController.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
